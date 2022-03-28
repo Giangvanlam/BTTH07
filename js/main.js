29f0ea347bcf549d6b5b2c413b9aa6e8a3dc92f3
BienAnhQuan <2051063783@e.tlu.edu.vn>
2022-03-28 21:19:34 +0700

    tôi thêm check_type và onlyOne

--- js/main.js
+++ js/main.js
@@ -19,3 +19,51 @@ function check_length(id, min_length, max_length, index_list_error) {
         }
     });
 }
+
+function check_type(id, value_check, message, index_list_error){
+	let div_right = document.querySelector(`.box-${id}`);
+	let new_element = document.createElement("p");
+	new_element.innerHTML = `${id} ${message}`;
+	new_element.style.color = 'red';
+	new_element.style.fontSize = '12px';
+	new_element.style.margin = '2px 0 0 22%';
+	document.getElementById(`${id}`).addEventListener('blur', function(){
+		if(this.value.length>0 && !value_check.test(this.value)){
+			this.style.color = "red";
+			this.style.borderColor = "red";
+			list_error[index_list_error] = false;
+			div_right.appendChild(new_element);
+		}
+		else{
+			this.style.color = "black";
+			this.style.borderColor = "black";
+			list_error[index_list_error] = true;
+			div_right.removeChild(new_element);
+		}
+	});
+}
+
+function onlyOne(checkbox) {
+    let checkboxes = document.getElementsByName('language')
+    let atLeastOneChecked = false;
+    checkboxes.forEach((item) => {
+        if (item !== checkbox)
+        	item.checked = false;
+    });
+
+    checkboxes.forEach((item) => {
+        if (item.checked === true)
+        	atLeastOneChecked = true;
+    });
+
+	if(atLeastOneChecked === true) {
+		checkboxes.forEach((item) =>{
+			item.required = false;
+		});
+	}
+	else{
+		checkboxes.forEach((item) =>{
+			item.required = true;
+		});
+	}
+}
